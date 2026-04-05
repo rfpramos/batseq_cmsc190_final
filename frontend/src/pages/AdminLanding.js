2ccf82306652b79cdd3b04e97577721852a5db68
Ramnick Francis P. Ramos <63172385+rfpramos@users.noreply.github.com>
2026-04-05 12:23:30 +0800

    feat: adding user tester logging in for CINTERLABS

--- frontend/src/pages/AdminLanding.js
+++ frontend/src/pages/AdminLanding.js
@@ -18,13 +18,7 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
 import AppAppBar from '../components/AppAppBar';
-import Hero from '../components/Hero';
-import LogoCollection from '../components/LogoCollection';
-import Highlights from '../components/Highlights';
-import Pricing from '../components/Pricing';
-import Features from '../components/Features';
-// import Testimonials from '../components/Testimonials';
-import FAQ from '../components/FAQ';
+
 import Footer from '../components/Footer';
 import getLPTheme from '../getLPTheme';
 
