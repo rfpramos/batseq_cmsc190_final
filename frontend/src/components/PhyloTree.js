98b0139e55d81b06740ba43d1e3674a2c9ea18c0
Ramnick Francis P. Ramos <63172385+rfpramos@users.noreply.github.com>
2026-08-10 03:37:23 +0800

    developing: deployment

--- frontend/src/components/PhyloTree.js
+++ frontend/src/components/PhyloTree.js
@@ -164,7 +164,7 @@ export default function PhyloTree() {
   }, []);
 
 return (
-    <div className="w-full max-w-3xl mx-auto">
+    <div>
         <h2 className="text-xl font-semibold mb-2 text-gray-900 dark:text-green-400">
             Phylogenetic Tree
         </h2>
